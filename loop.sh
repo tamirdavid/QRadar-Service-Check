c7060750538ab667ff9ec871fef97b51d7c84279
Tamir David <53215879+tamirdavid@users.noreply.github.com>
2020-09-23 18:35:31 +0300

    Add files via upload

--- loop.sh
+++ loop.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
-while IFS= read -r line; do
-   ssh root@$line systemctl -q is-active hostcontext &&\echo -n -e "hostcontext is up" >> hostcontext.txt||
-   \echo -n -e "hostcontext is down" >> hostcontext.txt && echo " $line" >> hostcontext.txt
-done < "$1"
 
-bash csvMaker.sh hostcontext.txt
+file=$1
+while IFS= read -r p || [ -n "$p" ]
+do
 
+if ssh -qn root@$p ps aux | grep -q hostcontext; then
+    echo -n -e "hostcontext is up" >> services.txt
+else
+    echo -n -e "hostcontext is down" >> services.txt 
+fi
+echo " $p" >> services.txt
+
+if ssh -qn root@$p ps aux | grep -q ecs-ec; then
+    echo -n -e "ecs-ec is up" >> services.txt
+else
+    echo -n -e "ecs-ec is down" >> services.txt
+fi
+echo " $p" >> services.txt
+
+if ssh -qn root@$p ps aux | grep -q ecs-ec-ingress; then
+    echo -n -e "ecs-ec-ingress is up" >> services.txt
+else
+    echo -n -e "ecs-ec-ingress is down" >> services.txt
+fi
+echo " $p" >> services.txt
+
+done < "$file"
+
+bash csvMaker.sh services.txt
